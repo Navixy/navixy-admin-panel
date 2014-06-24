0ad7039bb91c152dea8ba547022421292b359973
LiquidUkulele <liquidukulele@gmail.com>
2014-06-24 08:21:09 +0100

    "release-1.1.0"

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,6 +20,7 @@ module.exports = function (grunt) {
                             from: 'productionFiles/index.html',
                             to: 'index.html'
                         },
+                        'VERSION',
                         'PConfig.js',
                         'PConfig.example.js',
                         'locale/locale-en.js',
