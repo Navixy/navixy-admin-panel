691b80aee7e977b8d87f2643e0e5e668c3305e1e
LiquidUkulele <liquidukulele@gmail.com>
2018-02-26 13:52:08 +0500

    rsync deploy

---
+++ rsync-deploy.js
@@ -0,0 +1,27 @@
+var config = require('./deploy_profiles.json'),
+    sys = require('sys'),
+    exec = require('child_process').exec,
+    profile = process.argv[2],
+    username = process.argv[3];
+
+if (profile) {
+    var profileObject = config[profile];
+
+    if (profileObject) {
+        var command = "rsync -rvzc --exclude='PConfig.js' --include='/PConfig.example.js' ./build/panel/ " + username + '@' + profileObject.host + ':' + profileObject.remotePath;
+        console.log(command);
+
+        exec(command, function (error, stdout, stderr) {
+            sys.print('stdout: ' + stdout);
+            sys.print('stderr: ' + stderr);
+            if (error !== null) {
+                console.log('exec error: ' + error);
+            }
+        });
+    } else {
+        throw 'Profile not found';
+    }
+} else {
+    throw 'Profile must be specified';
+}
+
