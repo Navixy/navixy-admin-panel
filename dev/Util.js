ed90e5571761ac1648bdc31f58daab8aa39bdb4e
LiquidUkulele <liquidukulele@gmail.com>
2018-10-24 10:45:51 +0500

    Добавлен пакет navixy-permissions

--- dev/Util.js
+++ dev/Util.js
@@ -1,7 +1,19 @@
 Ext.define('Dev.Util', {
     alternateClassName: 'Util',
     singleton: true,
+    dependencies: ['NavixyPermissions'],
     getRequiredSeparator: function () {
         return '<span style="color:red">*</span>:';
-    }
+    },
+
+    navixyPermissions: function (action, feature) {
+        var permissionStore = NavixyPermissions.default;
+        var gisPackage = Ext.getStore('Dealer').getGisPackage();
+        var gisPackageRole = permissionStore.gisPackageToRoleConverter(gisPackage);
+        var roles = [gisPackageRole];
+        if (Locale.Manager.getLocale() === 'ru') {
+            roles.push('ru-user');
+        }
+        return permissionStore.can(roles, action, feature);
+    },
 });
