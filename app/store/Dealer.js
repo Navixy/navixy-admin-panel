402962c1d8491df18b1f1e46041aeb8b9709a480
LiquidUkulele <liquidukulele@gmail.com>
2019-03-06 13:30:34 +0500

    wip

--- app/store/Dealer.js
+++ app/store/Dealer.js
@@ -8,79 +8,83 @@ Ext.define('NavixyPanel.store.Dealer', {
     extend: 'Ext.data.Store',
     storeId: 'Dealer',
     fields: [
-        "parent_dealer_id",
-        "contract_type",
-        "effective_dealer_id",
-        "title",
-        "legal_name",
-        "active_amount",
-        "active_limit",
-        "locale",
-        "domain",
-        "favicon",
-        "logo",
-        "subpaas",
-        "seller_currency",
-        "demo_tariff",
-        "store_period",
+        'parent_dealer_id',
+        'contract_type',
+        'effective_dealer_id',
+        'title',
+        'legal_name',
+        'active_amount',
+        'active_limit',
+        'locale',
+        'domain',
+        'favicon',
+        'logo',
+        'subpaas',
+        'seller_currency',
+        'demo_tariff',
+        'store_period',
         {
-            name: "paas_activation_date",
+            name: 'paas_activation_date',
             type: 'date',
             format: 'Y-m-d'
         }, {
-            name: "tracker_tariff_end_date",
+            name: 'tracker_tariff_end_date',
             type: 'date',
             format: 'Y-m-d'
         }, {
-            name: "license_balance",
+            name: 'license_balance',
             defaultValue: 500
         }, {
-            name: "block_status",
-            defaultValue: "NOT_BLOCKED"
+            name: 'block_status',
+            defaultValue: 'NOT_BLOCKED'
         }, {
-            name: "tariff",
+            name: 'tariff',
             defaultValue: {}
         }
     ],
 
     setGoogleClientId: function (google_client_id) {
-        this.google_client_id = google_client_id;
+        this.google_client_id = google_client_id
     },
 
     isPremiumGis: function () {
-        return this.hasPremiumGis() || !!this.google_client_id;
+        return this.hasPremiumGis() || !!this.google_client_id
     },
 
     getGisPackage: function () {
-        var record = this.first();
-        var tariff = record && record.get("tariff");
+        var record = this.first()
+        var tariff = record && record.get('tariff')
         return tariff && tariff.gis_package
     },
 
     hasPremiumGis: function () {
         var record = this.first(),
-            tariff = record && record.get("tariff");
+            tariff = record && record.get('tariff')
 
-        return tariff && tariff.premium_gis;
+        return tariff && tariff.premium_gis
     },
 
     getImgUrl: function (field) {
         var record = this.first(),
             value = record.get(field),
-            isUrl = new RegExp('http://|https://', 'i').test(value);
+            isUrl = new RegExp('http://|https://', 'i').test(value)
 
         return value
             ? isUrl
-                   ? value
-                   : Ext.API.getGlobalApiUrl({action: value})
+                ? value
+                : Ext.API.getGlobalApiUrl({ action: value })
             : null
     },
 
     getLogo: function () {
-        return this.getImgUrl('logo') || false;
+        return this.getImgUrl('logo') || false
     },
 
     getFavicon: function () {
-        return this.getImgUrl('favicon') || false;
+        return this.getImgUrl('favicon') || false
+    },
+
+    isSubpaas: function () {
+        return this.first().get('subpaas')
     }
-});
+})
