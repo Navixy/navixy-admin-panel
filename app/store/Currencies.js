b6b84b76920ac9dcb60bffd4c11f74a665759131
LiquidUkulele <liquidukulele@gmail.com>
2021-04-02 14:11:45 +0500

    added PGK

--- app/store/Currencies.js
+++ app/store/Currencies.js
@@ -688,6 +688,11 @@ Ext.define('NavixyPanel.store.Currencies', {
                 type: "UGX",
                 name: _l.get('currency.ugx'),
                 priority: 5
+            },
+            {
+                type: "PGK",
+                name: _l.get('currency.pgk'),
+                priority: 5
             }
         ];
 
