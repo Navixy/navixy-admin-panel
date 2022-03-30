7fb28f19fe71cb3e67dbdfbb96b551cff204e7bd
LiquidUkulele <liquidukulele@gmail.com>
2022-03-30 13:08:55 +0300

    FR-6849: remove show model info checkbox from system preferencies tab

--- app/view/settings/Edit.js
+++ app/view/settings/Edit.js
@@ -923,12 +923,6 @@ Ext.define('NavixyPanel.view.settings.Edit', {
                 xtype: 'blockheader',
                 html: _l.get('settings.edit_form.misc_header')
             },
-            {
-                xtype: 'checkbox',
-                role: 'checkbox',
-                boxLabel: _l.get('settings.fields.display_model_features_link') + this.getHintSymbol(_l.get('settings.fields.display_model_features_link_hint')),
-                name: 'display_model_features_link'
-            },
             locale === 'en'
                 ? null
                 :
