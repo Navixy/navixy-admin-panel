b1fda0bf6d7222520325bac0d46af93645674788
LiquidUkulele <liquidukulele@gmail.com>
2023-05-24 06:21:42 +0000

    Update PConfig.example.sa.js

--- PConfig.example.sa.js
+++ PConfig.example.sa.js
@@ -3,7 +3,7 @@ Config = {
     apiProfiles: {
         // Java-API settings
         japi: {
-            apiRoot: 'http://api.domain.com/panel',
+            apiRoot: '/api-v2/panel',
             apiUrlTpl: ['{apiRoot}/{handler}/{action}']
         }
     },
@@ -11,40 +11,19 @@ Config = {
     // Links templates
     links: {
         // Link to "automatic" authorization in monitoring with user hash in GET.
-        monUrlTpl: ['http://{company_url}/pro/demo/?session_key={hash}'],
+        monUrlTpl: ['://{company_url}/pro/demo/?session_key={hash}'],
         // Link to "automatic" authorization in selected app with user hash in GET.
         appUrlTpl: ['//{company_url}/pro/demo/?session_key={hash}&app={app}&tracker_id={tracker_id}']
     },
 
     // web-socket connection address for devices terminal
-    terminalHost: 'ws://api.domain.com:8383/',
-    // Default domain name. Use for determining map types and other settings.
-    // Can be string (i.e. "gdemoi.ru") or array of strings (i.e.["gdemoi.ru", "gdemoi.com"].)
-    // Default value is ".navixy.ru"
-    // TO ignore comparing with  ".navixy.ru" set value to false
-    // All above will be ignored if server sends own options
-    paas_domain: '.domain.com',
-
-    // Incoming sms-gateway phone number for navixy
-    navixyInboundNumber: "79037976362",
+    terminalHost: 'ws'+window.location.protocol.substring(4)+'//'+window.location.host+'/',
+    
+    paas_domain: false,
+    
+    navixyInboundNumber: "unknown",
 
     //use google maps whithout its js api
     useGoogleMapsTilesDirectly: false
-    //Extended locales for use in MONITORING not in Panel
-    //extended_locales: {
-    //    override: true,
-    //    items: [{
-    //        id: 'en_US',
-    //        name: 'en',
-    //        text: 'English',
-    //        alias: ['en', 'en_US'],
-    //        rtl: false
-    //    }, {
-    //        id: 'zh_CN',
-    //        name: 'zh',
-    //        text: 'Chineese',
-    //        alias: ['zh', 'zh_CN'],
-    //        rtl: false
-    //    }]
-    //}
+    
 };
