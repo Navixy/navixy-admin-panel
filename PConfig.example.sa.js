0cdbdf78472636ba9a40a3e2abfbb18832b6937f
LiquidUkulele <liquidukulele@gmail.com>
2023-06-02 12:06:20 +0500

    Update PConfig.example.sa.js
    убрал опечатку

--- PConfig.example.sa.js
+++ PConfig.example.sa.js
@@ -11,19 +11,19 @@ Config = {
     // Links templates
     links: {
         // Link to "automatic" authorization in monitoring with user hash in GET.
-        monUrlTpl: ['://{company_url}/pro/demo/?session_key={hash}'],
+        monUrlTpl: ['//{company_url}/pro/demo/?session_key={hash}'],
         // Link to "automatic" authorization in selected app with user hash in GET.
         appUrlTpl: ['//{company_url}/pro/demo/?session_key={hash}&app={app}&tracker_id={tracker_id}']
     },
 
     // web-socket connection address for devices terminal
     terminalHost: 'ws'+window.location.protocol.substring(4)+'//'+window.location.host+'/',
-    
+
     paas_domain: false,
-    
+
     navixyInboundNumber: "unknown",
 
     //use google maps whithout its js api
     useGoogleMapsTilesDirectly: false
-    
+
 };
