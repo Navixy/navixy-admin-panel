000368c9d7598f86c3cf505edc8ac3c488980bf8
LiquidUkulele <liquidukulele@gmail.com>
2024-05-21 23:23:09 +0400

    FR-8986 Integrate menu editor using the iframe

--- app/view/settings/components/MenuEditorWindow.js
+++ app/view/settings/components/MenuEditorWindow.js
@@ -8,18 +8,42 @@ Ext.define('NavixyPanel.view.settings.components.MenuEditorWindow', {
   xtype: 'menu-editor-window',
   cls: 'menu-editor-window',
   closeAction: 'destroy',
-  hash: null,
+  hash: Ext.API.authKey,
   maximized: true,
   minimizable: false,
-  modal: false,
+  modal: true,
+  layout: 'fit',
+
+  getTexts: function () {
+    return {
+      windowTitle: _l.get('settings.edit_form.menu_editor'),
+    };
+  },
 
   getItems: function () {
+    var appLink = this.getApplicationLink();
+    var html = appLink ? '<iframe src="' + appLink + '"></iframe>' : '';
+
     return [
       {
-        xtype: 'panel',
-        ui: 'light',
-        html: '<div id="menu-editor-app"></div>',
+        xtype: 'component',
+        layout: 'fit',
+        html: html,
       },
     ];
+  },
+
+  getApplicationLink: function () {
+    var PAGE_NAME = 'MenuEditor';
+    var domain = Ext.getStore('Dealer').first().get('domain');
+    var data = Ext.apply({
+      company_url: domain,
+      hash: this.hash,
+      page: PAGE_NAME,
+    });
+    var locale = Locale.Manager.getLocale();
+    var link = new Ext.Template(Config.links.panelUrlTpl).apply(data);
+
+    return !!domain && Ext.String.urlAppend(link, 'locale=' + locale);
   },
 });
