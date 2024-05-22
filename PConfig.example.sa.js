561cf98300eebd00e68a84bfb7da52bb87b207ce
LiquidUkulele <liquidukulele@gmail.com>
2024-05-22 22:59:38 +0400

    FR-8986 Update example configs

--- PConfig.example.sa.js
+++ PConfig.example.sa.js
@@ -14,8 +14,8 @@ Config = {
         monUrlTpl: ['//{company_url}/pro/demo/?session_key={hash}'],
         // Link to "automatic" authorization in selected app with user hash in GET.
         appUrlTpl: ['//{company_url}/pro/demo/?session_key={hash}&app={app}&tracker_id={tracker_id}'],
-        // Link to the "menu editor" application
-        panelUrlTpl: ['//{company_url}/panel/wrapper?session_key={hash}&page={page}'],
+        // Link to the "new panel" page
+        panelUrlTpl: '/new-panel/#/{page}?panel_session_key={hash}',
     },
 
     // web-socket connection address for devices terminal
