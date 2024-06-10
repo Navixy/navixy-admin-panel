428592132979e8051a33b5a3181760b776a087bd
LiquidUkulele <liquidukulele@gmail.com>
2024-06-10 12:25:16 +0400

    FR-8986 Restore presets list

--- app/view/settings/Edit.js
+++ app/view/settings/Edit.js
@@ -938,21 +938,21 @@ Ext.define('NavixyPanel.view.settings.Edit', {
                 boxLabel: _l.get('settings.edit_form.device_settings_checkbox') + this.getHintSymbol(_l.get('settings.edit_form.device_settings_checkbox_hint'))
             },
 
-            // {
-            //     name: 'menu_preset_id',
-            //     xtype: 'combobox',
-            //     fieldLabel: _l.get('settings.fields.menu_preset'),
-            //     store: Ext.getStore('MenuPresets'),
-            //     displayField: 'title',
-            //     queryMode: 'local',
-            //     valueField: 'id',
-            //     listeners: {
-            //         scope: this,
-            //         change: function (cbx, value) {
-            //             this.record.set('menu_preset_id', value);
-            //         },
-            //     },
-            // },
+            {
+                name: 'menu_preset_id',
+                xtype: 'combobox',
+                fieldLabel: _l.get('settings.fields.menu_preset'),
+                store: Ext.getStore('MenuPresets'),
+                displayField: 'title',
+                queryMode: 'local',
+                valueField: 'id',
+                listeners: {
+                    scope: this,
+                    change: function (cbx, value) {
+                        this.record.set('menu_preset_id', value);
+                    },
+                },
+            },
 
             {
                 xtype: 'button',
