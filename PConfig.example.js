ef806c1d830a4be5f1e976e8575d5b43b21bf860
LiquidUkulele <liquidukulele@gmail.com>
2024-09-11 14:14:51 +0400

    FR-9381 Fix missing commas

--- PConfig.example.js
+++ PConfig.example.js
@@ -54,7 +54,7 @@ Config = {
         styleParams: 'CLEAN_CART=ALL&CART=1&CARD=2&DESIGN_TYPE=1&CURRENCY=USD&SHORT_FORM=1&ORDERSTYLE=nLWo5ZTPiLk='
     },
     // use google maps whithout its js api ,
-    useGoogleMapsTilesDirectly: false
+    useGoogleMapsTilesDirectly: false,
 
     //google_key: {
     //    allow: true,
--- PConfig.example.sa.js
+++ PConfig.example.sa.js
@@ -23,7 +23,7 @@ Config = {
 
     paas_domain: false,
 
-    navixyInboundNumber: "unknown"
+    navixyInboundNumber: "unknown",
 
     // Simplifies email validation, i.e. will validate email+tag@domain.com
     enableSimpleEmailValidation: false,
